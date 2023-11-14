7edeb32b4fcc5b4bfb51886cf39d2e4892054019
Antropov Anton <antropov.a.g@gmail.com>
2023-11-14 14:00:29 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const yourName = "name";
+function display(text) {
+    console.log(text);
+}
+display(yourName);
+
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+const yourName: string = "name";
+
+function display(text: string): void {
+    console.log(text);
+}
+
+display(yourName);
