8d3a307c1892b214159d186d435d1d8bb22eff00
Antropov Anton <antropov.a.g@gmail.com>
2023-11-17 17:46:04 +0400

    Fix textarea style

--- main.js
+++ main.js
@@ -13,8 +13,20 @@ button.addEventListener("click", () => {
 });
 
 function autoExpand(textarea) {
+    const button_1 = document.querySelector(".comments__add-comment-button");
     textarea.style.height = "auto";
     textarea.style.height = textarea.scrollHeight + "px";
+    console.log(textarea.value.length);
+    if (textarea.value.length > 0) {
+
+        button_1.classList.remove("comments__add-comment-button_type_disabled");
+        button_1.removeAttribute("disabled");
+        button_1.classList.add("comments__add-comment-button_type_enabled");
+    } else if (textarea.value.length === 0 || textarea.value.length > 1000){
+        button_1.classList.remove("comments__add-comment-button_type_enabled");
+        button_1.setAttribute("disabled", "");
+        button_1.classList.add("comments__add-comment-button_type_disabled");
+    }
 }
 
 // document.addEventListener('DOMContentLoaded', () => {
